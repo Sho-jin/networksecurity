02cabfaee63e37b4d0a7d320c3c114a17599f593
Sho-jin <sjindal3_be23@thapar.edu>
2026-08-01 02:24:13 +0530

    Data Validation Commited

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.components.data_ingestion import DataIngestion
-from networksecurity.entity.config_entity import DataIngestionConfig
-from networksecurity.entity.config_entity import trainingPipelineConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,trainingPipelineConfig,DataValidationConfig
 
+from networksecurity.components.data_validation import DataValidation
 
 import sys
 
@@ -16,7 +16,16 @@ if __name__ =="__main__":
         logging.info("Initiate the data Ingestion")
 
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+
         print(data_ingestion_artifact)
+
+        logging.info("Initiate the dData Validation")
+
+        data_validation_config = DataValidationConfig(training_pipeline_config)
+        data_validation = DataValidation(data_ingestion_artifact,data_validation_config)
+        data_ingestion_artifact = data_validation.initiate_data_validation()
+
+
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
