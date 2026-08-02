7fa9cbfdcff3a142729eb98018eec2533fe4e213
Sho-jin <sjindal3_be23@thapar.edu>
2026-08-02 21:28:29 +0530

    Final Changes of Deployment

--- app.py
+++ app.py
@@ -75,6 +75,7 @@ async def predict_route(request: Request, file: UploadFile = File(...)) :
         print(df["predicted_column"])
         os.makedirs("prediction_output", exist_ok=True)
         df.to_csv("prediction_output/output.csv", index=False)
+        print("Prediction output saved to prediction_output/output.csv")
 
         table_html = df.to_html(classes= "table table-striped ", index=False)
         return templates.TemplateResponse("table.html", {"request": request, "table": table_html})
